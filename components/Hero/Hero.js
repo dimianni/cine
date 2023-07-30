adab28357fcf6bad9e2ab4c002f949e99a62d8a1
Dmytro Anikin <dimianni@users.noreply.github.com>
2023-07-30 22:31:50 +0200

    Revert "Revert "Implemented Pagination""
    This reverts commit 6e87e7ab03d4a5bb1a7da12809919bd9dcc56e3b.

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
             <div className="hero-bg absolute top-0 left-0 h-full w-full bg-cover -z-10"></div>
             <div className="w-full h-full flex flex-col items-center justify-center">
                 <h1 className="text-3xl lg:text-6xl font-semibold text-center my-4 text-white">Find and Save movies <br /> to your collection!</h1>
-                <Link href="/search">
+                <Link href="/search?genre=all&year=all&title=&page=1">
                     <button className="bg-green py-2 px-3 rounded-md">Get Started</button>
                 </Link>
             </div>
--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,42 @@
 import MovieCard from "@/components/MovieCard/MovieCard";
 import SearchFilters from "@/components/SearchFilters/SearchFilters";
+import Pagination from "@/utils/Pagination";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Search(){
 
+    const router = useRouter();
+    const { genre, year, title, page } = router.query;
+
     const [searchedMovies, setSearchedMovies] = useState(null);
+    const [pagesCount, setPagesCount] = useState(null)
+
+    async function getMovies(genre, year, title, page) {
+        const response = await fetch('api/getSearchedMovies', {
+            method: "POST",
+            body: JSON.stringify({ genre, year, title, page }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const { movies, totalPages } = await response.json()
+        setSearchedMovies(movies)
+        setPagesCount(totalPages)
+    }
+
+    function clearSearch(){
+        setSearchedMovies(null)
+    }
 
     useEffect(() => {
-        console.log(searchedMovies);
-    }, [searchedMovies])
+        
+        getMovies(genre, year, title, page)
+
+        return () => {
+            clearSearch()
+        }
+    }, [genre, year, title, page])
 
     return (
         <div className="container">
@@ -28,9 +56,10 @@ export default function Search(){
                         </ul>
                     ) : (
                         <p>Loading...</p>
-                    )
-                        
-                    }
+                    )}
+
+                    { pagesCount > 1 && <Pagination numOfPages={pagesCount} currentPage={parseInt(page)} genre={genre} year={year} title={title} />}
+                    
                 </section>
             </main>
         </div>
